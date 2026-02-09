4767d66cc0a2f681659162b513681cdfe666a483
Kofi Oduro <kofia.oduro@gmail.com>
2026-02-09 12:50:06 -0500

    Added a new title to my app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "This is kofi testing his code"
+    return "Testing my code to see if it triggers automatically"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
