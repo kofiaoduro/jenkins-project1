d39ad98d910c511ed030a89274744e303ac2bd87
Kofi Oduro <kofia.oduro@gmail.com>
2026-02-09 14:18:24 -0500

    Just made another change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello coach Sadip How are you Doing. I hope you are welll"
+    return "Hello coach Sadip How are you Doing. I jjust made another change"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
